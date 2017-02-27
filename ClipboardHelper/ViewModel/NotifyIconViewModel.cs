dac195536dc5bcbc43e8adef1e7908a761d7789c
Atte Tuomisto <at.tuomisto@gmail.com>
2017-02-27 11:28:25 +0200

    Added the previous copy command in systray menu, does not work yet.

--- ClipboardHelper/ViewModel/NotifyIconViewModel.cs
+++ ClipboardHelper/ViewModel/NotifyIconViewModel.cs
@@ -6,78 +6,93 @@ using ClipboardHelper.View;
 namespace ClipboardHelper.ViewModel
 {
     class NotifyIconViewModel
+    {
+        /// <summary>
+        /// Shows a window, if none is already open.
+        /// </summary>
+        public ICommand ShowWindowCommand
         {
-            /// <summary>
-            /// Shows a window, if none is already open.
-            /// </summary>
-            public ICommand ShowWindowCommand
+            get
             {
-                get
+                return new DelegateCommand
                 {
-                    return new DelegateCommand
+                    CanExecuteFunc = () => Application.Current.MainWindow.IsVisible == false || Application.Current.MainWindow.WindowState == WindowState.Minimized,
+                    CommandAction = () =>
                     {
-                        CanExecuteFunc = () => Application.Current.MainWindow.IsVisible == false || Application.Current.MainWindow.WindowState == WindowState.Minimized,
-                        CommandAction = () =>
-                        {
                             //Application.Current.MainWindow = new MainWindow();
                             Application.Current.MainWindow.Show();
-                            Application.Current.MainWindow.WindowState = WindowState.Normal;
-                        }
-                    };
-                }
+                        Application.Current.MainWindow.WindowState = WindowState.Normal;
+                    }
+                };
             }
+        }
 
-            /// <summary>
-            /// Hides the main window. This command is only enabled if a window is open.
-            /// </summary>
-            public ICommand HideWindowCommand
+        /// <summary>
+        /// Hides the main window. This command is only enabled if a window is open.
+        /// </summary>
+        public ICommand HideWindowCommand
+        {
+            get
             {
-                get
-                {
                 return new DelegateCommand
                 {
                     CommandAction = () => Application.Current.MainWindow.Hide(),
                     CanExecuteFunc = () => Application.Current.MainWindow.IsVisible == true
-                    };
-                }
-            }
-
-
-            /// <summary>
-            /// Shuts down the application.
-            /// </summary>
-            public ICommand ExitApplicationCommand
-            {
-                get
-                {
-                    return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
-                }
+                };
             }
         }
 
 
         /// <summary>
-        /// Simplistic delegate command for the demo.
+        /// Shuts down the application.
         /// </summary>
-        public class DelegateCommand : ICommand
+        public ICommand ExitApplicationCommand
         {
-            public Action CommandAction { get; set; }
-            public Func<bool> CanExecuteFunc { get; set; }
-
-            public void Execute(object parameter)
+            get
             {
-                CommandAction();
-            }
-
-            public bool CanExecute(object parameter)
-            {
-                return CanExecuteFunc == null || CanExecuteFunc();
-            }
-
-            public event EventHandler CanExecuteChanged
-            {
-                add { CommandManager.RequerySuggested += value; }
-                remove { CommandManager.RequerySuggested -= value; }
+                return new DelegateCommand
+                {
+                    CommandAction = () => Application.Current.Shutdown()
+                };
             }
         }
     }
+
+    /// <summary>
+    /// Gets the previous copy from clipboard manager
+    /// </summary>
+    public ICommand PreviousCopyCommand
+    {
+        get
+        {
+            //TODO: Do the functionality for the previous copy
+            return new DelegateCommand { CommandAction = () => Application.Current.Shutdown() };
+        }
+    }
+
+
+    /// <summary>
+    /// Simplistic delegate command for the demo.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
